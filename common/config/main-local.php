4f7ea1a4b6b78a598963f8280474bcfb8d7eab08
Anchisa Nuchmit <anchisa.nu@mail.wu.ac.th>
2023-03-22 21:49:13 +0700

    commit file that ignore.

---
+++ common/config/main-local.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+    'components' => [
+        // 'db' => [
+        //     'class' => 'yii\db\Connection',
+        //     'dsn' => 'mysql:host=localhost;dbname=yii2advanced',
+        //     'username' => 'root',
+        //     'password' => '',
+        //     'charset' => 'utf8',
+        // ],
+        'mongodb' => [
+            'class' => '\yii\mongodb\Connection',
+            'dsn' => 'mongodb://root:1234@127.0.0.1:27017',
+            'defaultDatabaseName'=>'yii2'
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'viewPath' => '@common/mail',
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
+            'useFileTransport' => true,
+        ],
+    ],
+];
